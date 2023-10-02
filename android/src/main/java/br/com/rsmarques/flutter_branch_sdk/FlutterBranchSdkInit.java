2ec5b72b24ee15a74b1f40b8a0050f9a4352aae2
Helder <helderhsr@msn.com>
2023-10-02 09:25:54 -0300

    update with guilherme code

--- android/src/main/java/br/com/rsmarques/flutter_branch_sdk/FlutterBranchSdkInit.java
+++ android/src/main/java/br/com/rsmarques/flutter_branch_sdk/FlutterBranchSdkInit.java
@@ -8,10 +8,12 @@ import io.branch.referral.Branch;
 public class FlutterBranchSdkInit {
     private static final String DEBUG_NAME = "FlutterBranchSDK";
     private static final String PLUGIN_NAME = "Flutter";
-    private static final String PLUGIN_VERSION = "6.8.0";
+    private static final String PLUGIN_VERSION = "6.7.1";
 
     public static void init(Context context) {
         ApplicationInfoHelper applicationInfoHelper = new ApplicationInfoHelper(context);
+
+        Branch.expectDelayedSessionInitialization(true);
 
         if (applicationInfoHelper.getEnableLog()) {
             LogUtils.debug(DEBUG_NAME, "Branch SDK with log enable");
@@ -20,8 +22,12 @@ public class FlutterBranchSdkInit {
             Log.i(DEBUG_NAME, "Branch SDK with out log");
         }
 
+        if (applicationInfoHelper.getEnableFacebookAds()) {
+             Branch.getAutoInstance(context).enableFacebookAppLinkCheck();
+        }
+
         // Branch object initialization
         Branch.registerPlugin(PLUGIN_NAME, PLUGIN_VERSION);
-        Branch.getAutoInstance(context);
+//         Branch.getAutoInstance(context);
     }
 }
